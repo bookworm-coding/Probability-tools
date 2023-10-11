5412973d4c96ffc538f4350b6e2150265e4b39bb
Kangmin Kim <88320744+bookworm-coding@users.noreply.github.com>
2023-10-11 22:10:53 +0000

    소스 코드 URL 추가

--- 홈.py
+++ 홈.py
@@ -12,3 +12,4 @@ st.set_page_config(
 st.title("확률 도구")
 st.subheader("다양한 확률 그래프 등을 제공합니다.")
 st.write("Made by 김강민")
+st.write("[소스 코드 Github 주소](https://github.com/bookworm-coding/Probability_test)")
