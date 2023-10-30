a202c0015d91bc71133ef44cd5eac963b21fe1e8
bookworm-coding <bookwormkkm00@gmail.com>
2023-10-30 17:55:24 +0900

    multipage 폴더명을 modules로 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ show_pages(
     [
         Page("streamlit_app.py", "확률 도구", "🏠"),
         Section("경우의 수 문제", "📁"),
-        Page("multipage/제로게임 모든 경우들 열거.py", "제로게임 모든 경우 나열", ""),
-        Page("multipage/제로게임 경우의 수.py", "제로게임 경우의 수", ""),
+        Page("modules/제로게임 모든 경우들 열거.py", "제로게임 모든 경우 나열", ""),
+        Page("modules/제로게임 경우의 수.py", "제로게임 경우의 수", ""),
         Section("단순한 확률 문제", "📁"),
-        Page("multipage/동전 던지기 확률.py", "동전 던지기 확률", "🪙"),
-        Page("multipage/가위바위보 확률.py", "가위바위보 확률", "✊"),
-        Page("multipage/주사위 확률.py", "주사위 확률", "🎲"),
+        Page("modules/동전 던지기 확률.py", "동전 던지기 확률", "🪙"),
+        Page("modules/가위바위보 확률.py", "가위바위보 확률", "✊"),
+        Page("modules/주사위 확률.py", "주사위 확률", "🎲"),
         Section("복잡한 확률 문제", "📁"),
-        Page("multipage/평년 생일 문제 확률.py", "평년 생일 문제 확률", "📅"),
-        Page("multipage/윤년 생일 문제 확률.py", "윤년 생일 문제 확률", "📅"),
+        Page("modules/평년 생일 문제 확률.py", "평년 생일 문제 확률", "📅"),
+        Page("modules/윤년 생일 문제 확률.py", "윤년 생일 문제 확률", "📅"),
     ]
 )
 
